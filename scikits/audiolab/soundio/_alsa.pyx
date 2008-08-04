06e9a7b44910b6c20147baed74c788664647f33c
David Cournapeau <cournape@gmail.com>
2008-08-04 11:29:40 +0000

    Add ability to play arrays of short for alsa backend.

--- scikits/audiolab/soundio/_alsa.pyx
+++ scikits/audiolab/soundio/_alsa.pyx
@@ -59,19 +59,33 @@ cdef extern from "alsa/asoundlib.h":
                 SND_PCM_FORMAT_IEC958_SUBFRAME
 
         ctypedef struct snd_pcm_t
+        # XXX: how to make sure the typedef is OK ?
+        ctypedef unsigned long snd_pcm_uframes_t
 
         int snd_pcm_open(snd_pcm_t **, char*, int, int)
         int snd_pcm_close(snd_pcm_t *)
+        int snd_pcm_drain(snd_pcm_t *)
 
         int snd_pcm_set_params(snd_pcm_t *, snd_pcm_format_t,
                         snd_pcm_access_t, unsigned int,
                         unsigned int, int, unsigned int)
+
+        int snd_pcm_writei(snd_pcm_t *, void*, snd_pcm_uframes_t)
 
         char* snd_strerror(int error)
 
         int snd_card_next(int *icard)
         int snd_card_get_name(int icard, char** name)
         char* snd_asoundlib_version()
+
+cdef extern from "numpy/arrayobject.h":
+        ctypedef int intp
+        ctypedef extern class numpy.ndarray [object PyArrayObject]:
+                cdef char *data
+                cdef int nd
+                cdef intp *dimensions
+                cdef intp *strides
+                cdef int flags
 
 cdef extern from "stdlib.h":
         ctypedef unsigned long size_t
@@ -80,6 +94,9 @@ cdef extern from "stdlib.h":
         void *realloc(void *ptr, size_t size)
         size_t strlen(char *s)
         char *strcpy(char *dest, char *src)
+
+cdef extern from "stdint.h":
+        ctypedef unsigned short int16_t
 
 cdef extern from "Python.h":
         object PyString_FromStringAndSize(char *v, int len)
@@ -116,7 +133,7 @@ def card_name(index):
                 free(sptr)
         return cardname
 
-cdef class PCM:
+cdef class _PCM:
         cdef snd_pcm_t* pcmhdl
         cdef public char* name
 
@@ -135,21 +152,46 @@ cdef class PCM:
                         snd_pcm_close(self.pcmhdl)
 
 cdef class Device:
-        cdef PCM pcm
+        cdef _PCM pcm
         cdef unsigned int samplerate
-        cdef unsigned int channels
+        # XXX: set property instead
+        cdef public unsigned int channels
+        cdef snd_pcm_format_t format
+        cdef snd_pcm_access_t access
         def __init__(self, samplerate = 48000, channels = 1,
                      format = SND_PCM_FORMAT_S16,
                      access = SND_PCM_ACCESS_RW_INTERLEAVED):
-                self.pcm = PCM()
+                self.pcm = _PCM()
 
                 self.samplerate = samplerate
                 self.channels = channels
+
+                self.access = access
+                self.format = format
 
                 st = snd_pcm_set_params(self.pcm.pcmhdl, format, access, channels,
                                         samplerate, 1, 1000000)
                 if st < 0:
                         raise AlsaException()
+
+        def play_short(self, ndarray input):
+                cdef int16_t* p = <int16_t*>input.data
+                cdef int16_t* buff = NULL
+                #cdef int n = input.dimensions[0]
+                cdef int n = input.dimensions[0]
+                cdef int nb = n / 1024 / 16
+                cdef snd_pcm_uframes_t bufsz = 1024 * 16
+
+                assert self.format == SND_PCM_FORMAT_S16
+                assert self.access == SND_PCM_ACCESS_RW_INTERLEAVED
+
+                for i from 0 <= i  < nb:
+                        st =  snd_pcm_writei(self.pcm.pcmhdl, &p[i * bufsz], bufsz)
+                        if not st == bufsz:
+                                print "Error while writing to device", st
+                                print snd_strerror(st)
+
+                snd_pcm_drain(self.pcm.pcmhdl)
 
         def _get_name(self):
                 return self.pcm.name
