1cf67723b482ed02f289a1cd754c7a765d5ab47c
David Cournapeau <cournape@gmail.com>
2008-08-04 09:53:39 +0000

    Add useful enum for alsa.

--- scikits/audiolab/soundio/_alsa.pyx
+++ scikits/audiolab/soundio/_alsa.pyx
@@ -1,7 +1,62 @@
 cdef extern from "alsa/asoundlib.h":
-        ctypedef enum SND_PCM_STREAM :
+        ctypedef enum snd_pcm_stream_t:
                SND_PCM_STREAM_PLAYBACK
                SND_PCM_STREAM_CAPTURE
+        ctypedef enum snd_pcm_access_t :
+                SND_PCM_ACCESS_MMAP_INTERLEAVED
+                SND_PCM_ACCESS_MMAP_NONINTERLEAVED
+                SND_PCM_ACCESS_MMAP_COMPLEX
+                SND_PCM_ACCESS_RW_INTERLEAVED
+                SND_PCM_ACCESS_RW_NONINTERLEAVED
+        ctypedef enum snd_pcm_format_t :
+                SND_PCM_FORMAT_UNKNOWN
+                SND_PCM_FORMAT_S8
+                SND_PCM_FORMAT_U8
+                SND_PCM_FORMAT_S16_LE
+                SND_PCM_FORMAT_S16_BE
+                SND_PCM_FORMAT_U16_LE
+                SND_PCM_FORMAT_U16_BE
+                SND_PCM_FORMAT_S24_LE
+                SND_PCM_FORMAT_S24_BE
+                SND_PCM_FORMAT_U24_LE
+                SND_PCM_FORMAT_U24_BE
+                SND_PCM_FORMAT_S32_LE
+                SND_PCM_FORMAT_S32_BE
+                SND_PCM_FORMAT_U32_LE
+                SND_PCM_FORMAT_U32_BE
+                SND_PCM_FORMAT_FLOAT_LE
+                SND_PCM_FORMAT_FLOAT_BE
+                SND_PCM_FORMAT_FLOAT64_LE
+                SND_PCM_FORMAT_FLOAT64_BE
+                SND_PCM_FORMAT_IEC958_SUBFRAME_LE
+                SND_PCM_FORMAT_IEC958_SUBFRAME_BE
+                SND_PCM_FORMAT_MU_LAW
+                SND_PCM_FORMAT_A_LAW
+                SND_PCM_FORMAT_IMA_ADPCM
+                SND_PCM_FORMAT_MPEG
+                SND_PCM_FORMAT_GSM
+                SND_PCM_FORMAT_SPECIAL
+                SND_PCM_FORMAT_S24_3LE
+                SND_PCM_FORMAT_S24_3BE
+                SND_PCM_FORMAT_U24_3LE
+                SND_PCM_FORMAT_U24_3BE
+                SND_PCM_FORMAT_S20_3LE
+                SND_PCM_FORMAT_S20_3BE
+                SND_PCM_FORMAT_U20_3LE
+                SND_PCM_FORMAT_U20_3BE
+                SND_PCM_FORMAT_S18_3LE
+                SND_PCM_FORMAT_S18_3BE
+                SND_PCM_FORMAT_U18_3LE
+                SND_PCM_FORMAT_U18_3BE
+                SND_PCM_FORMAT_S16
+                SND_PCM_FORMAT_U16
+                SND_PCM_FORMAT_S24
+                SND_PCM_FORMAT_U24
+                SND_PCM_FORMAT_S32
+                SND_PCM_FORMAT_U32
+                SND_PCM_FORMAT_FLOAT
+                SND_PCM_FORMAT_FLOAT64
+                SND_PCM_FORMAT_IEC958_SUBFRAME
         char* snd_strerror(int error)
         int snd_card_next(int *icard)
         int snd_card_get_name(int icard, char** name)
