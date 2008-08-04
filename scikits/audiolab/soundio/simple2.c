00e909c7021724ac6063e411d743c85ff6988323
David Cournapeau <cournape@gmail.com>
2008-08-04 11:29:03 +0000

    Improve simple example with pcm drain.

--- scikits/audiolab/soundio/simple2.c
+++ scikits/audiolab/soundio/simple2.c
@@ -3,6 +3,38 @@
 
 #include <alsa/asoundlib.h>
 
+int play(snd_pcm_t *pcm)
+{
+        int16_t buff[1024 * 16];
+        int i, j;
+        snd_pcm_uframes_t frames;
+
+        for (i = 0; i < 4; ++i) {
+            for (j = 0; j < 1024 * 16; ++j) {
+                buff[j] = random() & 0xff;
+            }
+            frames = snd_pcm_writei(pcm, buff, 1024 * 16); 
+            fprintf(stderr, "%lu\n", frames);
+        }
+
+        return 0;
+}
+
+int set(snd_pcm_t *pcm, snd_pcm_format_t format, snd_pcm_access_t access, 
+        unsigned int channels, unsigned int samplerate)
+{
+        int st;
+
+        st = snd_pcm_set_params(pcm, format, access, channels, samplerate,
+                                1, 1000000);      
+        if (st < 0) {
+                printf("Output failed: %s\n", snd_strerror(st));
+                return st;
+        }
+
+        return 0;
+}
+
 int main()
 {
         snd_pcm_t *pcm;
@@ -13,7 +45,6 @@ int main()
         unsigned int nc = 1;
         snd_output_t *output;
 
-        uint8_t buff[1024 * 16];
         int i, j;
         snd_pcm_sframes_t frames;
 
@@ -29,30 +60,17 @@ int main()
                 return -1;
         }
 
-        st = snd_pcm_set_params(pcm, 
-                                SND_PCM_FORMAT_S16, 
-                                SND_PCM_ACCESS_RW_INTERLEAVED, 
-                                nc, 
-                                rrate,
-                                1,
-                                1000000);      
-        if (st < 0) {
-                printf("Output failed: %s\n", snd_strerror(st));
-                return -1;
-        }
-        snd_pcm_dump(pcm, output);
+        set(pcm, SND_PCM_FORMAT_S16, SND_PCM_ACCESS_RW_INTERLEAVED, 1, 44100);
 
-        fprintf(stderr, "SND_PCM_FORMAT_U8 : %d\n", SND_PCM_FORMAT_U8);
-        fprintf(stderr, "SND_PCM_FORMAT_S16 : %d\n", SND_PCM_FORMAT_S16);
-        fprintf(stderr, "SND_PCM_FORMAT_FLOAT : %d\n", SND_PCM_FORMAT_FLOAT);
-        fprintf(stderr, "SND_PCM_ACCESS_RW_INTERLEAVED : %d\n", SND_PCM_ACCESS_RW_INTERLEAVED);
-        for (i = 0; i < 16; ++i) {
-            for (j = 0; j < 1024 * 16; ++j) {
-                buff[j] = random() & 0xff;
-            }
-            frames = snd_pcm_writei(pcm, buff, 1024 * 16); 
-            fprintf(stderr, "%u\n", frames);
-        }
+        play(pcm);
+        snd_pcm_drain(pcm);
+
+        set(pcm, SND_PCM_FORMAT_S16, SND_PCM_ACCESS_RW_INTERLEAVED, 1, 8000);
+
+        play(pcm);
+
+        //snd_pcm_dump(pcm, output);
+
         snd_output_close(output);
         snd_pcm_close(pcm);
 
