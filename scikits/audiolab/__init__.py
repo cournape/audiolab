3b75a02f5b82d9f45dec2dd76af4b3bf1f9648ba
David Cournapeau <cournape@gmail.com>
2008-11-25 07:03:53 +0000

    Fix test and bench function to new nose-based numpy testing.

--- scikits/audiolab/__init__.py
+++ scikits/audiolab/__init__.py
@@ -30,6 +30,7 @@ from matapi import *
 
 __all__ = filter(lambda s:not s.startswith('_'),dir())
 
-from numpy.testing import NumpyTest
-def test():
-    return NumpyTest().test()
+from numpy.testing import Tester
+
+test = Tester().test
+bench = Tester().bench
