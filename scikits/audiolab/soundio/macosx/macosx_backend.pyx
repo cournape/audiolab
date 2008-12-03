66b6655810db6d5f450e6a3dd3ad9eebfe40795e
David Cournapeau <cournape@gmail.com>
2008-12-03 12:09:49 +0000

    Enumerate core audio devices in cython.

--- scikits/audiolab/soundio/macosx/macosx_backend.pyx
+++ scikits/audiolab/soundio/macosx/macosx_backend.pyx
@@ -2,8 +2,11 @@ cimport AudioHardware
 from AudioHardware cimport *
 cimport stdlib
 
+cdef extern from "Python.h":
+	object PyString_FromStringAndSize(char *v, int len)
+
 def yo():
-	cdef UInt32 sz, ndevices
+	cdef UInt32 sz, ndevices, i
 	cdef AudioDeviceID *devices
 	cdef char *name
 
@@ -11,3 +14,25 @@ def yo():
 	ndevices = sz / sizeof(AudioDeviceID)
 
 	print "Number of detected devices:", ndevices
+	devices = stdlib.malloc(sizeof(*devices) * ndevices)
+	res = []
+	try:
+		AudioHardwareGetProperty(kAudioHardwarePropertyDevices, &sz,
+				devices)
+
+		for i in range(ndevices):
+			AudioDeviceGetPropertyInfo(devices[i], 0, false,
+						   kAudioDevicePropertyDeviceName, &sz,
+						   NULL)
+			name = malloc(sz + 1)
+			AudioDeviceGetProperty(devices[i], 0, false,
+					kAudioDevicePropertyDeviceName, &sz,
+					name)
+			devicename = PyString_FromStringAndSize(name, sz+1)
+			free(name)
+			res.append(devicename)
+
+	finally:
+		stdlib.free(devices)
+
+	return devicename
