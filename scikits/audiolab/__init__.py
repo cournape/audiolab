1e0f4de549ec63c163f11733a0e4f40603652f56
David Cournapeau <cournape@gmail.com>
2008-12-04 06:24:40 +0000

    Remove old play code based on temp file.

--- scikits/audiolab/__init__.py
+++ scikits/audiolab/__init__.py
@@ -28,6 +28,7 @@ from pysndfile import supported_format, supported_endianness, \
 #        sdifread, wavwrite, aiffwrite, flacwrite, auwrite, sdifwrite
 from pysndfile.matapi import *
 
+from soundio import play
 __all__ = filter(lambda s:not s.startswith('_'),dir())
 
 from numpy.testing import Tester
