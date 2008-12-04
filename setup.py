ea3accb0fa7fb5e5edecb307f520e3a004ce7bbf
David Cournapeau <cournape@gmail.com>
2008-12-04 12:50:08 +0000

    Put most meta-data for setup in separate file, for sharing with paver.

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python
-# Last Change: Thu Dec 04 07:00 PM 2008 J
+# Last Change: Thu Dec 04 08:00 PM 2008 J
 
 # Copyright (C) 2006-2007 Cournapeau David <cournape@gmail.com>
 #
@@ -21,46 +21,9 @@
 #   - check how to handle cmd line build options with distutils and use
 #   it in the building process
 
-descr   = """ audiolab is a small python package to import data from audio
-files to numpy arrays and export data from numpy arrays to audio files. It uses
-libsndfile from Erik Castro de Lopo for the underlying IO, which supports many
-different audio formats: http://www.mega-nerd.com/libsndfile/
-
-For now, the python api for audio IO should be stable; a matlab-like API is
-also available for quick read/write (ala wavread, wavwrite, etc...). For 1.0
-release, I hope to add support for simple read/write to soundcard, to be able
-to record and listen to data in numpy arrays.
-
-2006-2007, David Cournapeau
-
-LICENSE: audiolab is licensed under the LGPL, as is libsndfile itself. See
-COPYING.txt for details.  """
-
 from os.path import join
 import os
 import sys
-
-DISTNAME            = 'scikits.audiolab'
-DESCRIPTION         = 'A python module to make noise from numpy arrays'
-LONG_DESCRIPTION    = descr
-MAINTAINER          = 'David Cournapeau',
-MAINTAINER_EMAIL    = 'david@ar.media.kyoto-u.ac.jp',
-URL                 = 'http://www.ar.media.kyoto-u.ac.jp/members/david/softwares/audiolab',
-LICENSE             = 'LGPL'
-DOWNLOAD_URL        = URL
-
-MAJOR = 0
-MINOR = 9
-MICRO = 0
-DEV = True
-
-CLASSIFIERS = ['Development Status :: 4 - Beta',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: GNU Library or Lesser General '\
-        'Public License (LGPL)', 'Topic :: Multimedia :: Sound/Audio',
-        'Topic :: Scientific/Engineering']
 
 # The following is more or less random copy/paste from numpy.distutils ...
 import setuptools
@@ -68,21 +31,7 @@ import setuptools
 from distutils.errors import DistutilsError
 from numpy.distutils.core import setup
 
-def build_verstring():
-    return '%d.%d.%d' % (MAJOR, MINOR, MICRO)
-
-def build_fverstring():
-    if DEV:
-        return build_verstring() + 'dev'
-    else:
-        return build_verstring()
-
-def write_version(fname):
-    f = open(fname, "w")
-    f.writelines("version = '%s'\n" % build_verstring())
-    f.writelines("dev =%s\n" % DEV)
-    f.writelines("full_version = '%s'\n" % build_fverstring())
-    f.close()
+from release import *
 
 def configuration(parent_package='',top_path=None, package_name=DISTNAME):
     if os.path.exists('MANIFEST'): os.remove('MANIFEST')
