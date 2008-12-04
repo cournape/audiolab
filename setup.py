66dd7250ef4ade4030a587f2d1696834e32f6819
David Cournapeau <cournape@gmail.com>
2008-12-04 04:43:31 +0000

    Remove sndfile-related stuff from main setup.py, since it moved into audiolab.pysndfile.

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python
-# Last Change: Wed Dec 03 08:00 PM 2008 J
+# Last Change: Thu Dec 04 01:00 PM 2008 J
 
 # Copyright (C) 2006-2007 Cournapeau David <cournape@gmail.com>
 #
@@ -49,7 +49,6 @@ URL                 = 'http://www.ar.media.kyoto-u.ac.jp/members/david/softwares
 LICENSE             = 'LGPL'
 DOWNLOAD_URL        = URL
 
-SNDFILE_MAJ_VERSION = 1
 CLASSIFIERS = ['Development Status :: 4 - Beta', 
         'Environment :: Console',
         'Intended Audience :: Developers', 
@@ -60,106 +59,13 @@ CLASSIFIERS = ['Development Status :: 4 - Beta',
 
 # The following is more or less random copy/paste from numpy.distutils ...
 import setuptools
+
 from distutils.errors import DistutilsError
-from numpy.distutils.system_info import system_info, NotFoundError, dict_append, so_ext
-from numpy.distutils.core import setup, Extension
-
-class SndfileNotFoundError(NotFoundError):
-    """ sndfile (http://www.mega-nerd.com/libsndfile/) library not found.
-    Directories to search for the libraries can be specified in the
-    site.cfg file (section [sndfile])."""
-
-class sndfile_info(system_info):
-    #variables to override
-    section         = 'sndfile'
-    notfounderror   = SndfileNotFoundError
-    libname         = 'sndfile'
-    header          = 'sndfile.h'
-
-    def __init__(self):
-        system_info.__init__(self)
-
-    def library_extensions(self):
-        # We rewrite library_extension
-        exts = system_info.library_extensions(self)
-        if sys.platform == 'win32':
-            exts.insert(0, '.dll')
-        return exts
-
-    def calc_info(self):
-        """ Compute the informations of the library """
-        prefix  = 'lib'
-
-        # Look for the shared library
-        sndfile_libs    = self.get_libs('sndfile_libs', self.libname)
-        lib_dirs        = self.get_lib_dirs()
-        tmp             = None
-        for i in lib_dirs:
-            tmp = self.check_libs(i, sndfile_libs)
-            if tmp is not None:
-                info    = tmp
-                break
-        if tmp is None:
-            raise SndfileNotFoundError("sndfile library not found")
-
-        # Look for the header file
-        include_dirs    = self.get_include_dirs()
-        inc_dir         = None
-        for d in include_dirs:
-            p = self.combine_paths(d,self.header)
-            if p:
-                inc_dir     = os.path.dirname(p[0])
-                headername  = os.path.abspath(p[0])
-                break
-
-        if inc_dir is None:
-            raise SndfileNotFoundError("header not found")
-
-        if inc_dir is not None and tmp is not None:
-            if sys.platform == 'win32':
-                # win32 case
-                fullname    = prefix + tmp['libraries'][0] + \
-                        '.dll'
-            elif sys.platform == 'darwin':
-                # Mac Os X case
-                fullname    = prefix + tmp['libraries'][0] + '.' + \
-                        str(SNDFILE_MAJ_VERSION) + '.dylib'
-            else:
-                # All others (Linux for sure; what about solaris) ?
-                fullname    = prefix + tmp['libraries'][0] + '.so' + \
-                        '.' + str(SNDFILE_MAJ_VERSION)
-            fullname    = os.path.join(info['library_dirs'][0], fullname)
-            dict_append(info, include_dirs=[inc_dir],
-                    fullheadloc = headername,
-                    fulllibloc  = fullname)
-        else:
-            raise RuntimeError("This is a bug")
-
-        #print self
-        self.set_info(**info)
-        return
-
-from header_parser import do_subst_in_file
+from numpy.distutils.core import setup
 def configuration(parent_package='',top_path=None, package_name=DISTNAME):
     if os.path.exists('MANIFEST'): os.remove('MANIFEST')
-    if os.path.exists('scikits/audiolab/pysndfile.py'): os.remove('scikits/audiolab/pysndfile.py')
 
     pkg_prefix_dir = os.path.join('scikits', 'audiolab')
-    # Check that sndfile can be found and get necessary informations
-    # (assume only one header and one library file)
-    sf_info     = sndfile_info()
-    sf_config   = sf_info.get_info(2)
-    headername  = sf_config['fullheadloc']
-    libname     = sf_config['fulllibloc']
-
-    # Now, generate pysndfile.py.in
-    from generate_const import generate_enum_dicts
-    repdict = generate_enum_dicts(headername)
-    repdict['%SHARED_LOCATION%'] = libname
-    #do_subst_in_file('pysndfile.py.in', 'pysndfile.py', repdict)
-    do_subst_in_file(os.path.join(pkg_prefix_dir, 'pysndfile.py.in'),
-            os.path.join(pkg_prefix_dir, 'pysndfile.py'),
-            repdict)
 
     # Get the version
     from scikits.audiolab.info import VERSION as audiolab_version
