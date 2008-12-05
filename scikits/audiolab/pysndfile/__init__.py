87df9686444c46796475da06c67eeca01c4a46cc
David Cournapeau <cournape@gmail.com>
2008-12-05 07:30:10 +0000

    Use compat module instead of ctypes-based implementation.

--- scikits/audiolab/pysndfile/__init__.py
+++ scikits/audiolab/pysndfile/__init__.py
@@ -1,5 +1,4 @@
-from pysndfile import formatinfo, sndfile
+from _sndfile import Sndfile, Format, available_file_formats, available_encodings
+from compat import formatinfo, sndfile, PyaudioException, PyaudioIOError
 from pysndfile import supported_format, supported_endianness, \
                                        supported_encoding
-from pysndfile import PyaudioException, PyaudioIOError
-from _sndfile import Sndfile, Format, available_file_formats, available_encodings
