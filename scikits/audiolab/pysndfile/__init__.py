a1a41b1538d2277978636418c5806891d82d5cd2
David Cournapeau <cournape@gmail.com>
2008-12-05 07:29:04 +0000

    Put available_ functions in main audiolab namespace.

--- scikits/audiolab/pysndfile/__init__.py
+++ scikits/audiolab/pysndfile/__init__.py
@@ -2,4 +2,4 @@ from pysndfile import formatinfo, sndfile
 from pysndfile import supported_format, supported_endianness, \
                                        supported_encoding
 from pysndfile import PyaudioException, PyaudioIOError
-from _sndfile import Sndfile, Format
+from _sndfile import Sndfile, Format, available_file_formats, available_encodings
