2d8c9b4400dc9ace1332baebf2a7b8559e4c2d39
David Cournapeau <cournape@gmail.com>
2008-12-06 17:20:50 +0000

    Remove broken example.

--- docs/src/examples/obsolete/format2.py
+++
@@ -1,6 +0,0 @@
-from scikits.audiolab import supported_format, supported_encoding, \
-            supported_endianness
-
-print supported_format()
-print supported_encoding()
-print supported_endianness()
