cd33046e4980bf40393fdb480641693cff41edc3
David Cournapeau <cournape@gmail.com>
2008-12-06 14:41:08 +0000

    Do not import ctypes-based sndfile wrapper anymore.

--- scikits/audiolab/pysndfile/__init__.py
+++ scikits/audiolab/pysndfile/__init__.py
@@ -1,4 +1,3 @@
 from _sndfile import Sndfile, Format, available_file_formats, available_encodings
 from compat import formatinfo, sndfile, PyaudioException, PyaudioIOError
-from pysndfile import supported_format, supported_endianness, \
-                                       supported_encoding
+from compat import supported_format, supported_endianness, supported_encoding
