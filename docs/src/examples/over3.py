b6e3824730239e24fc39fabeb39c27d87434a34f
David Cournapeau <cournape@gmail.com>
2008-12-06 17:21:21 +0000

    Fix one more example.

--- docs/src/examples/over3.py
+++ docs/src/examples/over3.py
@@ -1,3 +1,5 @@
+from scikits.audiolab import Format, Sndfile
+
 # Use 24 bits encoding, big endian
 format = Format('wav', 'pcm24', 'big')
-f = Sndfile(filename, 'write', format, 2, 48000)
+f = Sndfile('foo.wav', 'write', format, 2, 48000)
