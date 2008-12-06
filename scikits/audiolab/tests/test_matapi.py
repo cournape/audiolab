7223a1193a921113925047a9e930f77ff2cd4d2f
David Cournapeau <cournape@gmail.com>
2008-12-06 14:42:42 +0000

    Use skipif instead of exception for flac/ogg matlab-like funcs.

--- scikits/audiolab/tests/test_matapi.py
+++ scikits/audiolab/tests/test_matapi.py
@@ -7,12 +7,17 @@ from tempfile import mkstemp
 from numpy.testing import *
 import numpy as N
 
-from scikits.audiolab import wavread, auread, aiffread, sdifread, flacread
-from scikits.audiolab import wavwrite, auwrite, aiffwrite, sdifwrite, flacwrite
+from scikits.audiolab import wavread, auread, aiffread, sdifread, flacread, \
+                             oggread
+from scikits.audiolab import wavwrite, auwrite, aiffwrite, sdifwrite, flacwrite, \
+                             oggwrite
 from scikits.audiolab import PyaudioException
 from scikits.audiolab import Sndfile, Format as audio_format
+from scikits.audiolab import available_file_formats
 
 from testcommon import open_tmp_file, close_tmp_file
+
+AVAILABLE_FORMATS = available_file_formats()
 
 class test_audiolab(TestCase):
     def _test_read(self, func, format, filext):
@@ -43,12 +48,17 @@ class test_audiolab(TestCase):
         """ Check wavread """
         self._test_read(wavread, audio_format('wav', 'pcm16', 'file'), 'wav')
 
+    @dec.skipif('flac' not in AVAILABLE_FORMATS,
+                "Skip flacread test, FLAC not supported by your sndfile")
     def test_flacread(self):
         """ Check flacread """
-        try:
-            self._test_read(flacread, audio_format('flac', 'pcm16', 'file'), 'flac')
-        except NotImplementedError:
-            print "Flac unsupported, flacread not tested"
+        self._test_read(flacread, audio_format('flac', 'pcm16', 'file'), 'flac')
+
+    @dec.skipif('ogg' not in AVAILABLE_FORMATS,
+                "Skip oggread test, OGG not supported by your sndfile")
+    def test_oggread(self):
+        """ Check oggread """
+        self._test_read(oggread, audio_format('ogg', 'vorbis', 'file'), 'ogg')
 
     def test_auread(self):
         """ Check auread """
@@ -86,7 +96,7 @@ class test_audiolab(TestCase):
             try:
                 rnoise  = wavread(cfilename)[0]
                 raise Exception("wavread on non wav file succeded, expected to fail")
-            except PyaudioException, e:
+            except ValueError, e:
                 pass
                 #print str(e) + ", as expected"
 
@@ -147,12 +157,17 @@ class test_audiolab(TestCase):
         """ Check wavwrite """
         self._test_write(sdifwrite, audio_format('ircam', 'pcm16', 'file'), 'sdif')
 
+    @dec.skipif('flac' not in AVAILABLE_FORMATS,
+                "Skip flacwrite test, FLAC not supported by your sndfile")
     def test_flacwrite(self):
         """ Check flacwrite """
-        try:
-            self._test_write(flacwrite, audio_format('flac', 'pcm16', 'file'), 'flac')
-        except FlacUnsupported:
-            print "Flac unsupported, flacwrite not tested"
+        self._test_write(flacwrite, audio_format('flac', 'pcm16', 'file'), 'flac')
+
+    @dec.skipif('ogg' not in AVAILABLE_FORMATS,
+                "Skip oggwrite test, OGG not supported by your sndfile")
+    def test_oggwrite(self):
+        """ Check oggwrite """
+        self._test_write(oggwrite, audio_format('ogg', 'vorbis', 'file'), 'ogg')
 
 if __name__ == "__main__":
     NumpyTest().run()
