72573dc239f1cc1d0452cad09762a369d9076dcf
David Cournapeau <cournape@gmail.com>
2008-12-06 17:21:05 +0000

    Fix play example.

--- docs/src/examples/play.py
+++ docs/src/examples/play.py
@@ -4,4 +4,4 @@ data, fs, enc = wavread('test.wav')
 # There is a discrepency between wavread (one column per channel) and play
 # convention (one row per channel). Audiolab will be fully converted to 'numpy
 # conventions' (last axis per default) after version 0.9
-play(data.T, rate=fs)
+play(data.T, fs)
