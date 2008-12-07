6005426b1dfa0c641817e9526cb86fe87c304bdd
David Cournapeau <cournape@gmail.com>
2008-12-07 07:39:39 +0000

    Simple C program to output sound on Core Audio.

--- scikits/audiolab/soundio/macosx/enumerate.c
+++ scikits/audiolab/soundio/macosx/enumerate.c
@@ -55,23 +55,23 @@ static int prop()
 {
 	UInt32 sz, buffer_size;
 	AudioDeviceID odevice;
-    AudioStreamBasicDescription  ostreamdesc;
+	AudioStreamBasicDescription  ostreamdesc;
 	OSStatus st;
 
 	/* Get default output device */
 	sz = sizeof(AudioDeviceID);
 	st = AudioHardwareGetProperty(
-		kAudioHardwarePropertyDefaultOutputDevice,
-		&sz, &odevice);
+			kAudioHardwarePropertyDefaultOutputDevice,
+			&sz, &odevice);
 
 	if (odevice == kAudioDeviceUnknown) {
-        	fprintf(stderr, "odevice is kAudioDeviceUnknown\n");
+		fprintf(stderr, "odevice is kAudioDeviceUnknown\n");
 		return -1;
 	}
 
 	/* Get stream properties of output device */
 	sz = sizeof(ostreamdesc);
-    	st = AudioDeviceGetProperty(odevice, 0, false,
+	st = AudioDeviceGetProperty(odevice, 0, false,
 			kAudioDevicePropertyStreamFormat, &sz,
 			&ostreamdesc);
 	if (st) {
@@ -121,44 +121,58 @@ static int prop()
 		return -1;
 	} ;
 
-    return 0;
+	return 0;
 }
 
 typedef struct {
-    int done;
+	int remaining;
 } user_data;
 
 static OSStatus callback(AudioDeviceID device, 
-    const AudioTimeStamp* current_time, 
-    const AudioBufferList* data_in, 
-    const AudioTimeStamp* time_in,
-    AudioBufferList* data_out, 
-    const AudioTimeStamp* time_out, 
-    void* client_data)
+		const AudioTimeStamp* current_time, 
+		const AudioBufferList* data_in, 
+		const AudioTimeStamp* time_in,
+		AudioBufferList* data_out, 
+		const AudioTimeStamp* time_out, 
+		void* client_data)
 {
-    printf("done: %d\n", ((user_data*)client_data)->done);
-    ((user_data*)client_data)->done = 1;
-    return noErr;
+	int sz, i, nsamples;
+	float *data;
+	printf("remaining: %d\n", ((user_data*)client_data)->remaining);
+
+	sz = (data_out->mBuffers)[0].mDataByteSize ;
+	nsamples = sz / sizeof (float) ;
+	data = (float*)((data_out->mBuffers)[0].mData);
+
+	fprintf(stderr, "%ld samples\n", nsamples);
+	if (NCHANNELS > 1) {
+		for(i = nsamples - 1; i >= 0; --i) {
+			data[i] = 2 * random() / ((float)RAND_MAX) - 1;
+		}
+	} else {
+		fprintf(stderr, "mono not supported yet\n");
+	}
+	((user_data*)client_data)->remaining -= 1;
+	return noErr;
 }
 
 int main()
 {
 	AudioDeviceID odevice;
-    AudioStreamBasicDescription  ostreamdesc;
-    UInt32 sz, buffer_size;
-    OSStatus st;
-    user_data d;
+	AudioStreamBasicDescription  ostreamdesc;
+	UInt32 sz, buffer_size;
+	OSStatus st;
+	user_data d;
 
-    d.done = 0;
 #if 0
 	enumerate_devices();
-    prop();
+	prop();
 #endif
 
 	/* Get default output device */
 	sz = sizeof(AudioDeviceID);
-    st = AudioHardwareGetProperty(kAudioHardwarePropertyDefaultOutputDevice,
-            &sz, &odevice);
+	st = AudioHardwareGetProperty(kAudioHardwarePropertyDefaultOutputDevice,
+			&sz, &odevice);
 
 	if (odevice == kAudioDeviceUnknown) {
         fprintf(stderr, "odevice is kAudioDeviceUnknown\n");
@@ -167,57 +181,59 @@ int main()
 
 	/* Get stream properties of output device */
 	sz = sizeof(ostreamdesc);
-    st = AudioDeviceGetProperty(odevice, 0, false,
-            kAudioDevicePropertyStreamFormat, &sz,
-            &ostreamdesc);
+	st = AudioDeviceGetProperty(odevice, 0, false,
+			kAudioDevicePropertyStreamFormat, &sz,
+			&ostreamdesc);
 	if (st) {
 		fprintf(stderr, "error while getting stream format\n");
 		return -1;
 	}
-    if (ostreamdesc.mFormatID != kAudioFormatLinearPCM) {
-        fprintf(stderr, "Not linear pcm\n");
-        return -1;
-    }
-    printf("channels: %lu, samplerate %f\n", ostreamdesc.mChannelsPerFrame,
-            ostreamdesc.mSampleRate);
-    ostreamdesc.mChannelsPerFrame = NCHANNELS;
-    ostreamdesc.mSampleRate = SAMPLERATE;
-    printf("channels: %lu, samplerate %f\n", ostreamdesc.mChannelsPerFrame,
-            ostreamdesc.mSampleRate);
-    
-    st = AudioDeviceSetProperty(odevice, NULL, 0, false,
-            kAudioDevicePropertyStreamFormat, sizeof (AudioStreamBasicDescription),
-            &ostreamdesc);
-    if (st != noErr) {
-        fprintf(stderr, "Error setting stream props\n");
-        return -1;
-    }
+	if (ostreamdesc.mFormatID != kAudioFormatLinearPCM) {
+		fprintf(stderr, "Not linear pcm\n");
+		return -1;
+	}
+	printf("channels: %lu, samplerate %f\n", ostreamdesc.mChannelsPerFrame,
+			ostreamdesc.mSampleRate);
+	ostreamdesc.mChannelsPerFrame = NCHANNELS;
+	ostreamdesc.mSampleRate = SAMPLERATE;
+	printf("channels: %lu, samplerate %f\n", ostreamdesc.mChannelsPerFrame,
+			ostreamdesc.mSampleRate);
 
-    /* get the buffersize that the default device uses for IO */
-    sz = sizeof (UInt32);
-    st = AudioDeviceGetProperty(odevice, 0, false,
-            kAudioDevicePropertyBufferSize, &sz, &buffer_size);
-    if (st) {
-        fprintf(stderr, "Error getting buffer size of default output\n");
-        return -1;
-    }
+	st = AudioDeviceSetProperty(odevice, NULL, 0, false,
+			kAudioDevicePropertyStreamFormat, sizeof
+			(AudioStreamBasicDescription),
+			&ostreamdesc);
+	if (st != noErr) {
+		fprintf(stderr, "Error setting stream props\n");
+		return -1;
+	}
 
-    st = AudioDeviceAddIOProc(odevice, callback, &d);
-    if (st) {
-        fprintf(stderr, "Error setting callback\n");
-        return -1;
-    }
+	/* get the buffersize that the default device uses for IO */
+	sz = sizeof (UInt32);
+	st = AudioDeviceGetProperty(odevice, 0, false,
+			kAudioDevicePropertyBufferSize, &sz, &buffer_size);
+	if (st) {
+		fprintf(stderr, "Error getting buffer size of default output\n");
+		return -1;
+	}
+	fprintf(stderr, "Buffer size is %d bytes\n", buffer_size);
 
-    st = AudioDeviceStart(odevice, callback);
-    if	(st != noErr) {
-        fprintf(stderr, "Error starting device \n");
-        return -1;
-    }
+	st = AudioDeviceAddIOProc(odevice, callback, &d);
+	if (st) {
+		fprintf(stderr, "Error setting callback\n");
+		return -1;
+	}
 
-    while (d.done == 0) {
-        usleep (10 * 1000);
-    }
-    fprintf(stderr, "Main: done: %d\n", d.done);
+	st = AudioDeviceStart(odevice, callback);
+	if	(st != noErr) {
+		fprintf(stderr, "Error starting device \n");
+		return -1;
+	}
+
+	d.remaining = 100;
+	while (d.remaining > 0) {
+		usleep (10 * 1000);
+	}
 
 	return 0;
 }
