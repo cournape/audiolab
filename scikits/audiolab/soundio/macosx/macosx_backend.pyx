674db366adaadb7be1ee0b36a982851d69ac457e
David Cournapeau <cournape@gmail.com>
2008-12-07 04:15:34 +0000

    Calling callback does not work yet on core audio backend.

--- scikits/audiolab/soundio/macosx/macosx_backend.pyx
+++ scikits/audiolab/soundio/macosx/macosx_backend.pyx
@@ -32,37 +32,107 @@
 cimport AudioHardware
 from AudioHardware cimport *
 cimport stdlib
+cimport numpy as cnp
 
 cdef extern from "Python.h":
-	object PyString_FromStringAndSize(char *v, int len)
+    object PyString_FromStringAndSize(char *v, int len)
+    cdef usleep(int)
 
 def yo():
-	cdef UInt32 sz, ndevices, i
-	cdef AudioDeviceID *devices
-	cdef char *name
+    cdef UInt32 sz, ndevices, i
+    cdef AudioDeviceID *devices
+    cdef char *name
 
-	AudioHardwareGetPropertyInfo(kAudioHardwarePropertyDevices, &sz, NULL)
-	ndevices = sz / sizeof(AudioDeviceID)
+    AudioHardwareGetPropertyInfo(kAudioHardwarePropertyDevices, &sz, NULL)
+    ndevices = sz / sizeof(AudioDeviceID)
 
-	devices = <AudioDeviceID*>stdlib.malloc(sizeof(AudioDeviceID) * ndevices)
-	res = []
-	try:
-		AudioHardwareGetProperty(kAudioHardwarePropertyDevices, &sz,
-				devices)
+    devices = <AudioDeviceID*>stdlib.malloc(sizeof(AudioDeviceID) * ndevices)
+    res = []
+    try:
+        AudioHardwareGetProperty(kAudioHardwarePropertyDevices, &sz,
+                devices)
 
-		for i in range(ndevices):
-			AudioDeviceGetPropertyInfo(devices[i], 0, False,
-						   kAudioDevicePropertyDeviceName, &sz,
-						   NULL)
-			name = <char*>stdlib.malloc(sz + 1)
-			AudioDeviceGetProperty(devices[i], 0, False,
-					kAudioDevicePropertyDeviceName, &sz,
-					name)
-			devicename = PyString_FromStringAndSize(name, sz+1)
-			stdlib.free(name)
-			res.append(devicename)
+        for i in range(ndevices):
+            AudioDeviceGetPropertyInfo(devices[i], 0, False,
+                           kAudioDevicePropertyDeviceName, &sz,
+                           NULL)
+            name = <char*>stdlib.malloc(sz + 1)
+            AudioDeviceGetProperty(devices[i], 0, False,
+                    kAudioDevicePropertyDeviceName, &sz,
+                    name)
+            devicename = PyString_FromStringAndSize(name, sz+1)
+            stdlib.free(name)
+            res.append(devicename)
 
-	finally:
-		stdlib.free(devices)
+    finally:
+        stdlib.free(devices)
 
-	return res
+    return res
+
+cdef OSStatus callback(AudioDeviceID device, AudioTimeStamp* current_time,
+	AudioBufferList* data_in, AudioTimeStamp* time_in,
+	AudioBufferList* data_out, AudioTimeStamp* time_out,
+	void* client_data):
+    cdef int sz, sample_count, read_count, k
+    cdef OSStatus st = 0
+
+    sz = (data_out[0]).mBuffers[0].mDataByteSize
+    print sz, (<int*>client_data)[0]
+
+    return st
+
+def play(cnp.ndarray input):
+    cdef UInt32 sz, buffer_size
+    cdef AudioDeviceID odevice
+    cdef OSStatus st
+    cdef AudioStreamBasicDescription ostreamdesc
+    cdef int a, b, c, d
+    cdef int done
+
+    # Get default output device
+    sz = sizeof(AudioDeviceID)
+    st = AudioHardwareGetProperty(kAudioHardwarePropertyDefaultOutputDevice, &sz,
+                                  &odevice)
+    if st:
+        raise RuntimeError("Error getting default output properties")
+
+    # get the buffersize that the default device uses for IO
+    sz = sizeof (UInt32)
+    st = AudioDeviceGetProperty(odevice, 0, False,
+            kAudioDevicePropertyBufferSize,
+            &sz, &buffer_size)
+    if st:
+        raise RuntimeError("Error getting buffer size of default output")
+
+    # Get default output stream format
+    sz = sizeof(ostreamdesc)
+    st = AudioDeviceGetProperty(odevice, 0, False,
+        kAudioDevicePropertyStreamFormat, &sz,
+        &ostreamdesc)
+
+    print "=================="
+    print "sampling rate:", ostreamdesc.mSampleRate
+    print "channels:", ostreamdesc.mChannelsPerFrame
+
+    a = (<int>(ostreamdesc.mFormatID & 0xff000000) >> 24)
+    b = (<int>(ostreamdesc.mFormatID & 0x00ff0000) >> 16)
+    c = (<int>(ostreamdesc.mFormatID & 0x0000ff00) >> 8)
+    d = (<int>(ostreamdesc.mFormatID & 0x000000ff) >> 0)
+    print "format ID: %c%c%c%c" % (a, b, c, d)
+
+    if ostreamdesc.mFormatID != kAudioFormatLinearPCM:
+        raise RuntimeError("Not linear pcm")
+
+    done = 0
+    st = AudioDeviceAddIOProc (odevice, callback, <void*>(&done))
+    if st:
+        raise RuntimeError("error setting cllback")
+
+    st = AudioDeviceStart (odevice, callback)
+    if st:
+        raise RuntimeError("error starting ")
+
+    done = 0
+    print done
+    usleep(10000)
+    print done
