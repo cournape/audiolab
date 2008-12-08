eccd3c6f09c6e0b180bc9200edb68dcdbf9b93ab
David Cournapeau <cournape@gmail.com>
2008-12-08 07:11:53 +0000

    Handle frameworks in system info factory.

--- scikits/audiolab/soundio/setuphelp.py
+++ scikits/audiolab/soundio/setuphelp.py
@@ -3,7 +3,8 @@ from distutils import log
 
 from numpy.distutils.system_info import system_info, NotFoundError, dict_append, so_ext
 
-def info_factory(name, libnames, headers, section=None, classname=None):
+def info_factory(name, libnames, headers, frameworks=None, 
+                 section=None, classname=None):
     """Create a system_info class.
 
     Parameters
@@ -27,6 +28,8 @@ def info_factory(name, libnames, headers, section=None, classname=None):
         classname = '%s_info' % name
     if not section:
         section = name
+    if not frameworks:
+        framesworks = []
 
     class _ret(system_info):
         def __init__(self):
@@ -37,33 +40,45 @@ def info_factory(name, libnames, headers, section=None, classname=None):
 
         def calc_info(self):
             """ Compute the informations of the library """
-            # Look for the shared library
             libs = self.get_libs('libraries', '')
-            lib_dirs = self.get_lib_dirs()
-            tmp = None
-            for d in lib_dirs:
-                tmp = self.check_libs(d, libnames)
-                if tmp is not None:
-                    info    = tmp
-                    break
-            if tmp is None:
-                return
+            if libs:
+                # Look for the shared library
+                lib_dirs = self.get_lib_dirs()
+                tmp = None
+                for d in lib_dirs:
+                    tmp = self.check_libs(d, libnames)
+                    if tmp is not None:
+                        info    = tmp
+                        break
+                if tmp is None:
+                    return
 
-            # Look for the header file
-            include_dirs = self.get_include_dirs()
-            inc_dir = None
-            for d in include_dirs:
-                p = self.combine_paths(d, headers)
-                if p:
-                    inc_dir = os.path.dirname(p[0])
-                    dict_append(info, include_dirs=[d])
-                    break
+                # Look for the header file
+                include_dirs = self.get_include_dirs()
+                inc_dir = None
+                for d in include_dirs:
+                    p = self.combine_paths(d, headers)
+                    if p:
+                        inc_dir = os.path.dirname(p[0])
+                        dict_append(info, include_dirs=[d])
+                        break
 
-            if inc_dir is None:
-                log.info('  %s not found' % name)
-                return
+                if inc_dir is None:
+                    log.info('  %s not found' % name)
+                    return
 
-            self.set_info(**info)
+                self.set_info(**info)
+            else:
+                # Look for frameworks
+                if frameworks:
+                    fargs = []
+                    for f in frameworks:
+                        p = "/System/Library/Frameworks/%s.framework" % f
+                        if os.path.exists(p):
+                            fargs.append("-framework")
+                            fargs.append(f)
+                    if fargs:
+                        self.set_info(extra_link_args=fargs)
             return
 
     _ret.__name__ = classname
