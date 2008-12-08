a85bbd1eace623545cec0528cc4abdd54d4d52b8
David Cournapeau <cournape@gmail.com>
2008-12-08 06:43:48 +0000

    Use a class factory for sndfile info; put the factory in separate file for easy sharing.

--- scikits/audiolab/pysndfile/setup.py
+++ scikits/audiolab/pysndfile/setup.py
@@ -1,83 +1,10 @@
 import os
 import sys
 
-from numpy.distutils.system_info import system_info, NotFoundError, dict_append, so_ext
 from numpy.distutils.core import setup, Extension
+from setuphelp import info_factory, NotFoundError
 
 SNDFILE_MAJ_VERSION = 1
-
-class SndfileNotFoundError(NotFoundError):
-    """ sndfile (http://www.mega-nerd.com/libsndfile/) library not found.
-    Directories to search for the libraries can be specified in the
-    site.cfg file (section [sndfile])."""
-
-class sndfile_info(system_info):
-    #variables to override
-    section         = 'sndfile'
-    notfounderror   = SndfileNotFoundError
-    libname         = 'sndfile'
-    header          = 'sndfile.h'
-
-    def __init__(self):
-        system_info.__init__(self)
-
-    def library_extensions(self):
-        # We rewrite library_extension
-        exts = system_info.library_extensions(self)
-        if sys.platform == 'win32':
-            exts.insert(0, '.dll')
-        return exts
-
-    def calc_info(self):
-        """ Compute the informations of the library """
-        prefix  = 'lib'
-
-        # Look for the shared library
-        sndfile_libs    = self.get_libs('sndfile_libs', self.libname)
-        lib_dirs        = self.get_lib_dirs()
-        tmp             = None
-        for i in lib_dirs:
-            tmp = self.check_libs(i, sndfile_libs)
-            if tmp is not None:
-                info    = tmp
-                break
-        if tmp is None:
-            raise SndfileNotFoundError("sndfile library not found")
-
-        # Look for the header file
-        include_dirs    = self.get_include_dirs()
-        inc_dir         = None
-        for d in include_dirs:
-            p = self.combine_paths(d,self.header)
-            if p:
-                inc_dir     = os.path.dirname(p[0])
-                headername  = os.path.abspath(p[0])
-                break
-
-        if inc_dir is None:
-            raise SndfileNotFoundError("header not found")
-
-        if inc_dir is not None and tmp is not None:
-            if sys.platform == 'win32':
-                # win32 case
-                fullname    = prefix + tmp['libraries'][0] + \
-                        '.dll'
-            elif sys.platform == 'darwin':
-                # Mac Os X case
-                fullname    = prefix + tmp['libraries'][0] + '.' + \
-                        str(SNDFILE_MAJ_VERSION) + '.dylib'
-            else:
-                # All others (Linux for sure; what about solaris) ?
-                fullname    = prefix + tmp['libraries'][0] + '.so' + \
-                        '.' + str(SNDFILE_MAJ_VERSION)
-            fullname    = os.path.join(info['library_dirs'][0], fullname)
-            dict_append(info, include_dirs=[inc_dir])
-        else:
-            raise RuntimeError("This is a bug")
-
-        #print self
-        self.set_info(**info)
-        return
 
 def configuration(parent_package='',top_path=None):
     from numpy.distutils.misc_util import Configuration
@@ -87,8 +14,15 @@ def configuration(parent_package='',top_path=None):
         os.remove('pysndfile.py')
 
     # Check that sndfile can be found and get necessary informations
-    sf_info     = sndfile_info()
-    sf_config   = sf_info.get_info(2)
+    sf_info = info_factory('sndfile', ['sndfile'], ['sndfile.h'],
+                           classname='SndfileInfo')()
+    try:
+        sf_config = sf_info.get_info(2)
+    except NotFoundError:
+        raise NotFoundError("""\
+sndfile (http://www.mega-nerd.com/libsndfile/) library not found.
+Directories to search for the libraries can be specified in the
+site.cfg file, in section [sndfile].""")
 
     confgr.add_extension('_sndfile', ['_sndfile.c'], extra_info=sf_config)
 
---
+++ scikits/audiolab/pysndfile/setuphelp.py
@@ -0,0 +1,72 @@
+import os
+from distutils import log
+
+from numpy.distutils.system_info import system_info, NotFoundError, dict_append, so_ext
+
+def info_factory(name, libnames, headers, section=None, classname=None):
+    """Create a system_info class.
+
+    Parameters
+    ----------
+        name : str
+            name of the library
+        libnames: seq
+            list of libraries to look for
+        headers : seq
+            list of headers to look for
+        classname : str
+            name of the returned class
+        section : str
+            section name in the site.cfg
+
+    Returns
+    -------
+        a system_info-derived class with the given meta-parameters
+    """
+    if not classname:
+        classname = '%s_info' % name
+    if not section:
+        section = name
+
+    class _ret(system_info):
+        def __init__(self):
+            system_info.__init__(self)
+
+        def library_extensions(self):
+            return system_info.library_extensions(self)
+
+        def calc_info(self):
+            """ Compute the informations of the library """
+            # Look for the shared library
+            libs = self.get_libs('libraries', '')
+            lib_dirs = self.get_lib_dirs()
+            tmp = None
+            for d in lib_dirs:
+                tmp = self.check_libs(d, libnames)
+                if tmp is not None:
+                    info    = tmp
+                    break
+            if tmp is None:
+                return
+
+            # Look for the header file
+            include_dirs = self.get_include_dirs()
+            inc_dir = None
+            for d in include_dirs:
+                p = self.combine_paths(d, headers)
+                if p:
+                    inc_dir = os.path.dirname(p[0])
+                    dict_append(info, include_dirs=[d])
+                    break
+
+            if inc_dir is None:
+                log.info('  %s not found' % name)
+                return
+
+            self.set_info(**info)
+            return
+
+    _ret.__name__ = classname
+    _ret.section = section
+    return _ret
+
