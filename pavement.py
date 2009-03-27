b86317e39a32a3f3174573dcd9f516d44428baa3
David Cournapeau <cournape@gmail.com>
2009-03-27 17:32:45 +0900

    Update paver script -> merge changes from samplerate's one, to get wine-build working and co.

--- pavement.py
+++ pavement.py
@@ -18,24 +18,35 @@ except ImportError, e:
 
 import paver
 import paver.doctools
-from paver.easy import Bunch, options, task, needs, dry, sh
+import paver.path
+from paver.easy import options, Bunch, task, needs, dry, sh, call_task
 from paver.setuputils import setup
 
 import common
 
-setup(
-    name=common.DISTNAME,
-    namespace_packages=['scikits'],
-    packages=setuptools.find_packages(),
-    install_requires=common.INSTALL_REQUIRE,
-    version=common.VERSION,
-    include_package_data=True,
-)
+PDF_DESTDIR = paver.path.path('docs') / 'pdf'
+HTML_DESTDIR = paver.path.path('docs') / 'html'
 
-options(
-        sphinx=Bunch(builddir="build", sourcedir="src"),
-        virtualenv=Bunch(script_name="install/bootstrap.py")
-)
+# Wine config for win32 builds
+WINE_SITE_CFG = """\
+[sndfile]
+library_dirs = /home/david/.wine/drive_c/local/lib
+include_dirs = /home/david/.wine/drive_c/local/include
+libraries = sndfile,vorbis,vorbisenc,FLAC,ogg,wsock32
+"""
+WINE_PY25 = "/home/david/.wine/drive_c/Python25/python.exe"
+WINE_PY26 = "/home/david/.wine/drive_c/Python26/python.exe"
+WINE_PYS = {'2.6' : WINE_PY26, '2.5': WINE_PY25}
+
+setup(name=common.DISTNAME,
+        namespace_packages=['scikits'],
+        packages=setuptools.find_packages(),
+        install_requires=common.INSTALL_REQUIRE,
+        version=common.VERSION,
+        include_package_data=True)
+
+options(sphinx=Bunch(builddir="build", sourcedir="src"),
+        virtualenv=Bunch(script_name="install/bootstrap.py"))
 
 def macosx_version():
     st = subprocess.Popen(["sw_vers"], stdout=subprocess.PIPE)
@@ -56,11 +67,12 @@ def mpkg_name():
 VPYEXEC = "install/bin/python"
 
 @task
-@needs('paver.virtual.bootstrap')
 def bootstrap():
     """create virtualenv in ./install"""
-    # XXX: fix the mkdir
-    sh('mkdir -p install')
+    install = paver.path.path('install')
+    if not install.exists():
+        install.mkdir()
+    call_task('paver.virtual.bootstrap')
     sh('cd install; %s bootstrap.py' % sys.executable)
 
 @task
@@ -70,41 +82,84 @@ def test_install():
     sh('%s setup.py install' % VPYEXEC)
 
 @task
-def build_version_files(options):
-    from common import write_version
-    write_version(os.path.join("scikits", "audiolab", "version.py"))
-    if os.path.exists(os.path.join("docs", "src")):
-        write_version(os.path.join("docs", "src", "audiolab_version.py"))
+def clean():
+    """Remove build, dist, egg-info garbage."""
+    d = ['build', 'dist', 'scikits.audiolab.egg-info', HTML_DESTDIR,
+            PDF_DESTDIR]
+    for i in d:
+        paver.path.path(i).rmtree()
+
+    (paver.path.path('docs') / options.sphinx.builddir).rmtree()
 
 @task
-#@needs(['latex', 'html'])
+def clean_bootstrap():
+    paver.path.path('install').rmtree()
+
+@task
+@needs("setuptools.bdist_mpkg", "doc")
 def dmg():
-    builddir = path("build") / "dmg"
+    pyver = ".".join([str(i) for i in sys.version_info[:2]])
+    builddir = paver.path.path("build") / "dmg"
     builddir.rmtree()
     builddir.mkdir()
 
     # Copy mpkg into image source
     mpkg_n = mpkg_name()
-    mpkg = path("dist") / mpkg_n
+    mpkg = paver.path.path("dist") / mpkg_n
     mpkg.copytree(builddir / mpkg_n)
+    tmpkg = builddir / mpkg_n
+    tmpkg.rename(builddir / ("audiolab-%s-py%s.mpkg" % (common.build_fverstring(), pyver)))
 
     # Copy docs into image source
-    doc_root = path(builddir) / "docs"
-    html_docs = path("docs") / "html"
-    pdf_docs = path("docs") / "pdf" / "audiolab.pdf"
+    doc_root = paver.path.path(builddir) / "docs"
+    html_docs = paver.path.path("docs") / "html"
+    pdf_docs = paver.path.path("docs") / "pdf" / "audiolab.pdf"
     html_docs.copytree(doc_root / "html")
     pdf_docs.copy(doc_root / "audiolab.pdf")
 
     # Build the dmg
     image_name = "audiolab-%s.dmg" % common.build_fverstring()
-    image = path(image_name)
+    image = paver.path.path(image_name)
     image.remove()
     cmd = ["hdiutil", "create", image_name, "-srcdir", str(builddir)]
-    subprocess.Popen(cmd)
+    sh(" ".join(cmd))
 #options.setup.package_data =
 #    setuputils.find_package_data("scikits/audiolab",
 #                                 package="scikits/audiolab",
 #                                 only_in_packages=False)
+
+@task
+def bdist_wininst_26():
+    _bdist_wininst(pyver='2.6')
+
+@task
+def bdist_wininst_25():
+    _bdist_wininst(pyver='2.5')
+
+@task
+@needs('bdist_wininst_25', 'bdist_wininst_26')
+def bdist_wininst():
+    pass
+
+@task
+@needs('clean', 'bdist_wininst')
+def winbin():
+    pass
+
+def _bdist_wininst(pyver):
+    site = paver.path.path('site.cfg')
+    exists = site.exists()
+    try:
+        if exists:
+            site.move('site.cfg.bak')
+        a = open(str(site), 'w')
+        a.writelines(WINE_SITE_CFG)
+        a.close()
+        sh('%s setup.py build -c mingw32 bdist_wininst' % WINE_PYS[pyver])
+    finally:
+        site.remove()
+        if exists:
+            paver.path.path('site.cfg.bak').move(site)
 
 if paver.doctools.has_sphinx:
     def _latex_paths():
@@ -127,49 +182,45 @@ if paver.doctools.has_sphinx:
     @task
     @needs('build_version_files')
     def latex():
-        """Build Audiolab's documentation and install it into
+        """Build audiolab's documentation and install it into
         scikits/audiolab/docs"""
         paths = _latex_paths()
         sphinxopts = ['', '-b', 'latex', paths.srcdir, paths.latexdir]
         dry("sphinx-build %s" % (" ".join(sphinxopts),), sphinx.main, sphinxopts)
+
+    @task
+    @needs('latex')
+    def pdf():
+        paths = _latex_paths()
         def build_latex():
             subprocess.call(["make", "all-pdf"], cwd=paths.latexdir)
         dry("Build pdf doc", build_latex)
-        destdir = paver.path.path("docs") / "pdf"
-        destdir.rmtree()
-        destdir.makedirs()
+        PDF_DESTDIR.rmtree()
+        PDF_DESTDIR.makedirs()
         pdf = paths.latexdir / "audiolab.pdf"
-        pdf.move(destdir)
+        pdf.copy(PDF_DESTDIR)
 
     @task
     @needs('build_version_files', 'paver.doctools.html')
-    def html_build():
-        """Build Audiolab's html documentation."""
-        pass
-
-    @task
-    @needs(['html_build'])
     def html():
-        """Build Audiolab's documentation and install it into
-        scikits/audiolab/docs"""
+        """Build audiolab documentation and install it into docs"""
         builtdocs = paver.path.path("docs") / options.sphinx.builddir / "html"
-        destdir = paver.path.path("docs") / "html"
-        destdir.rmtree()
-        builtdocs.move(destdir)
+        HTML_DESTDIR.rmtree()
+        builtdocs.copytree(HTML_DESTDIR)
 
     @task
-    @needs(['html', 'latex'])
+    @needs(['html', 'pdf'])
     def doc():
         pass
 
     @task
-    @needs('setuptools.command.sdist')
+    def build_version_files(options):
+        from common import write_version
+        write_version(os.path.join("scikits", "audiolab", "version.py"))
+        if os.path.exists(os.path.join("docs", "src")):
+            write_version(os.path.join("docs", "src", "audiolab_version.py"))
+    @task
+    @needs('html', 'pdf', 'setuptools.command.sdist')
     def sdist(options):
         """Build tarball."""
-        pass
-
-    @task
-    @needs(['doc', 'pavement.sdist'])
-    def release_sdist(options):
-        """Build doc + tarball."""
         pass
