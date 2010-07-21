834ca67ab8f7f599a83968c64892ae2173f2eeb6
David Cournapeau <cournape@gmail.com>
2010-07-21 02:40:57 +0200

    FEAT: add __sndfile_version__ version string.

--- scikits/audiolab/pysndfile/__init__.py
+++ scikits/audiolab/pysndfile/__init__.py
@@ -1,3 +1,4 @@
-from _sndfile import Sndfile, Format, available_file_formats, available_encodings
+from _sndfile import Sndfile, Format, available_file_formats, \
+        available_encodings, sndfile_version
 from compat import formatinfo, sndfile, PyaudioException, PyaudioIOError
 from compat import supported_format, supported_endianness, supported_encoding
